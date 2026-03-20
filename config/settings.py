29a2e4d537a6f2d991f20fd4492d9dd42e28ecbb
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-21 02:40:31 +0500

    Register procurement app in INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'apps.core',
     'apps.accounts',
     'apps.dashboard',
+    'apps.procurement',
 ]
 
 # ==============================================================================
