da18c814e6a93ca128a60e72847dc6994ae02cd3
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-26 02:36:09 +0500

    Add TMS AppConfig

---
+++ apps/tms/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TmsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.tms'
+    verbose_name = 'Transportation Management System'
