9320442a12e87692ca4644a5bfca06436cbe473c
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-27 01:53:52 +0500

    Add demand_planning app configuration

---
+++ apps/demand_planning/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DemandPlanningConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.demand_planning'
+    verbose_name = 'Demand Planning & Forecasting'
