5641f84a87b533f0de42d882a77651b71224dd38
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-28 03:49:50 +0500

    Add QMS app configuration

---
+++ apps/qms/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class QMSConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.qms'
+    verbose_name = 'Quality Management System'
