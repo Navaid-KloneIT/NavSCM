3a3d57d086a5519ad9bc673d52cee9f2ff8582df
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-01 00:33:57 +0500

    Add ColdChainConfig app configuration

---
+++ apps/cold_chain/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ColdChainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.cold_chain'
+    verbose_name = 'Cold Chain Management'
