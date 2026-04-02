16c8de79103d50befbd4f28e270748be5949c15f
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-02 17:03:19 +0500

    Add 3PL app config with verbose name

---
+++ apps/tpl/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TplConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.tpl'
+    verbose_name = '3PL Management'
